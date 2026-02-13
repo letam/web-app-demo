5eac3aa66b0f6c867c7cd64737e75372f4717919
Tam Le <tamsgit@gmail.com>
2026-02-12 19:23:15 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <List />
+    </>
+  )
+}
+
+
+function List() {
+  return <div>
+    {items.map(x => <ListItem value={x} />)}
+  </div>
+}
+
+function ListItem(props: {value: string}) {
+  return <div>{props.value}</div>
+}
+
+const items = [
+  'a',
+  'b',
+  'c',
+  'd',
+]
+
+export default App
